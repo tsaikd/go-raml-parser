248b4500db36f93d92dbf111e1bc442f339c23f7
tsaikd <tsaikd@gmail.com>
2016-05-29 15:19:58 +0800

    handle String type with default MaxLength
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- parser/String.go
+++ parser/String.go
@@ -7,22 +7,42 @@ type String struct {
 
 	// Minimum length of the string. Value MUST be equal to or greater than 0.
 	// Default: 0
-	MinLength Unimplement `yaml:"minLength" json:"minLength,omitempty"`
+	MinLength int64 `yaml:"minLength" json:"minLength,omitempty"`
 
 	// Maximum length of the string. Value MUST be equal to or greater than 0.
 	// Default: 2147483647
-	MaxLength Unimplement `yaml:"maxLength" json:"maxLength,omitempty"`
+	MaxLength int64 `yaml:"maxLength" json:"maxLength,omitempty"`
+}
+
+// UnmarshalYAML implement yaml unmarshaler
+func (t *String) UnmarshalYAML(unmarshaler func(interface{}) error) (err error) {
+	t.MaxLength = 2147483647
+
+	buf := struct {
+		Pattern   *string `yaml:"pattern" json:"pattern,omitempty"`
+		MinLength *int64  `yaml:"minLength" json:"minLength,omitempty"`
+		MaxLength *int64  `yaml:"maxLength" json:"maxLength,omitempty"`
+	}{}
+	if err = unmarshaler(&buf); err != nil {
+		return
+	}
+
+	if buf.Pattern != nil {
+		t.Pattern = *buf.Pattern
+	}
+	if buf.MinLength != nil {
+		t.MinLength = *buf.MinLength
+	}
+	if buf.MaxLength != nil {
+		t.MaxLength = *buf.MaxLength
+	}
+
+	return
 }
 
 // PostProcess for fill some field from RootDocument default config
 func (t *String) PostProcess(conf PostProcessConfig) (err error) {
 	if t == nil {
-		return
-	}
-	if err = t.MinLength.PostProcess(conf); err != nil {
-		return
-	}
-	if err = t.MaxLength.PostProcess(conf); err != nil {
 		return
 	}
 	return
@@ -31,6 +51,6 @@ func (t *String) PostProcess(conf PostProcessConfig) (err error) {
 // IsEmpty return true if String is empty
 func (t *String) IsEmpty() bool {
 	return t.Pattern == "" &&
-		t.MinLength.IsEmpty() &&
-		t.MaxLength.IsEmpty()
+		t.MinLength == 0 &&
+		t.MaxLength == 2147483647
 }
