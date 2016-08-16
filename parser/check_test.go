8fbbedc78566ee41097a4ed6a516c383292a59bc
tsaikd <tsaikd@gmail.com>
2016-08-16 12:24:34 +0800

    refine test case
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- parser/check_test.go
+++ parser/check_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/tsaikd/KDGoLib/jsonex"
-	"github.com/tsaikd/yaml"
 )
 
 func testCheckValueAPIType(apiType APIType, value interface{}, options ...CheckValueOption) (err error) {
@@ -18,179 +17,128 @@ func testCheckValueAPIType(apiType APIType, value interface{}, options ...CheckV
 }
 
 func Test_CheckValueAPIType_Boolean(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
-	apiType := APIType{}
-	apiType.setType(TypeBoolean)
-
-	err = testCheckValueAPIType(apiType, true)
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, 0)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, 0.1)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, "")
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{})
-	require.Error(err)
+	apiType := getAPITypeFromString(`
+type: boolean
+	`)
+	require.NoError(testCheckValueAPIType(apiType, true))
+	require.Error(testCheckValueAPIType(apiType, 0))
+	require.Error(testCheckValueAPIType(apiType, 0.1))
+	require.Error(testCheckValueAPIType(apiType, ""))
+	require.Error(testCheckValueAPIType(apiType, map[string]interface{}{}))
 }
 
 func Test_CheckValueAPIType_Integer(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
-	apiType := APIType{}
-	apiType.setType(TypeInteger)
-
-	err = testCheckValueAPIType(apiType, true)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, 0)
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, 0.1)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, "")
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{})
-	require.Error(err)
+	apiType := getAPITypeFromString(`
+type: integer
+	`)
+	require.Error(testCheckValueAPIType(apiType, true))
+	require.NoError(testCheckValueAPIType(apiType, 0))
+	require.Error(testCheckValueAPIType(apiType, 0.1))
+	require.Error(testCheckValueAPIType(apiType, ""))
+	require.Error(testCheckValueAPIType(apiType, map[string]interface{}{}))
 }
 
 func Test_CheckValueAPIType_Number(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
-	apiType := APIType{}
-	apiType.setType(TypeNumber)
-
-	err = testCheckValueAPIType(apiType, true)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, 0)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, 0, CheckValueOptionAllowIntegerToBeNumber(true))
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, 0.1)
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, "")
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{})
-	require.Error(err)
+	apiType := getAPITypeFromString(`
+type: number
+	`)
+	require.Error(testCheckValueAPIType(apiType, true))
+	require.Error(testCheckValueAPIType(apiType, 0))
+	require.NoError(testCheckValueAPIType(apiType, 0, CheckValueOptionAllowIntegerToBeNumber(true)))
+	require.NoError(testCheckValueAPIType(apiType, 0.1))
+	require.Error(testCheckValueAPIType(apiType, ""))
+	require.Error(testCheckValueAPIType(apiType, map[string]interface{}{}))
 }
 
 func Test_CheckValueAPIType_String(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
-	apiType := APIType{}
-	apiType.setType(TypeString)
-
-	err = testCheckValueAPIType(apiType, true)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, 0)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, 0.1)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, "")
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{})
-	require.Error(err)
+	apiType := getAPITypeFromString(`
+type: string
+	`)
+	require.Error(testCheckValueAPIType(apiType, true))
+	require.Error(testCheckValueAPIType(apiType, 0))
+	require.Error(testCheckValueAPIType(apiType, 0.1))
+	require.NoError(testCheckValueAPIType(apiType, ""))
+	require.Error(testCheckValueAPIType(apiType, map[string]interface{}{}))
 }
 
 func Test_CheckValueAPIType_Object(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
-	apiType := APIType{}
-	err = yaml.Unmarshal([]byte(strings.TrimSpace(`
+	apiType := getAPITypeFromString(`
 type: object
 properties:
     text: string
     int:  integer
     num:  number
-	`)), &apiType)
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, true)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, 0)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, 0.1)
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, "")
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{
-		"text": "",
-		"int":  0,
-		"num":  0.1,
-	})
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{
-		"text": "",
-		"int":  0,
-		"num":  0.1,
-	}, CheckValueOptionAllowRequiredPropertyToBeEmpty(true))
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{
-		"text": "test string",
-		"int":  0,
-		"num":  0.1,
-	})
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{
-		"text": 0,
-		"int":  0,
-		"num":  0.1,
-	})
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{
-		"text": "test string",
-		"int":  0.1,
-		"num":  0.1,
-	})
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{
-		"text": "test string",
-		"int":  0,
-		"num":  0,
-	})
-	require.Error(err)
-
-	err = testCheckValueAPIType(apiType, map[string]interface{}{
-		"text": "test string",
-		"int":  float64(0),
-		"num":  int64(0),
-	}, CheckValueOptionAllowIntegerToBeNumber(true))
-	require.NoError(err)
+	`)
+	require.Error(testCheckValueAPIType(apiType, true))
+	require.Error(testCheckValueAPIType(apiType, 0))
+	require.Error(testCheckValueAPIType(apiType, 0.1))
+	require.Error(testCheckValueAPIType(apiType, ""))
+	require.Error(testCheckValueAPIType(apiType,
+		map[string]interface{}{
+			"text": "",
+			"int":  0,
+			"num":  0.1,
+		},
+	))
+	require.NoError(testCheckValueAPIType(apiType,
+		map[string]interface{}{
+			"text": "",
+			"int":  0,
+			"num":  0.1,
+		},
+		CheckValueOptionAllowRequiredPropertyToBeEmpty(true),
+	))
+	require.NoError(testCheckValueAPIType(apiType,
+		map[string]interface{}{
+			"text": "test string",
+			"int":  0,
+			"num":  0.1,
+		},
+	))
+	require.Error(testCheckValueAPIType(apiType,
+		map[string]interface{}{
+			"text": 0,
+			"int":  0,
+			"num":  0.1,
+		},
+	))
+	require.Error(testCheckValueAPIType(apiType,
+		map[string]interface{}{
+			"text": "test string",
+			"int":  0.1,
+			"num":  0.1,
+		},
+	))
+	require.Error(testCheckValueAPIType(apiType,
+		map[string]interface{}{
+			"text": "test string",
+			"int":  0,
+			"num":  0,
+		},
+	))
+	require.NoError(testCheckValueAPIType(apiType,
+		map[string]interface{}{
+			"text": "test string",
+			"int":  float64(0),
+			"num":  int64(0),
+		},
+		CheckValueOptionAllowIntegerToBeNumber(true),
+	))
 
 	num := float64(123)
 	require.True(float64(int64(num)) == num)
@@ -198,98 +146,94 @@ properties:
 	require.False(float64(int64(num)) == num)
 
 	valmap := map[string]interface{}{}
-	err = jsonex.Unmarshal([]byte(`{
+	err := jsonex.Unmarshal([]byte(`{
 		"text": "test string",
 		"int": 0,
 		"num": 0
 	}`), &valmap)
 	require.NoError(err)
-	err = testCheckValueAPIType(apiType, valmap, CheckValueOptionAllowIntegerToBeNumber(true))
-	require.NoError(err)
+	require.NoError(testCheckValueAPIType(apiType, valmap, CheckValueOptionAllowIntegerToBeNumber(true)))
 }
 
 func Test_CheckValueAPIType_Array(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
-	apiType := APIType{}
-	apiType.setType("string[]")
-
-	err = testCheckValueAPIType(apiType, []string{"text"})
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, nil)
-	require.Error(err)
-	err = testCheckValueAPIType(apiType, nil, CheckValueOptionAllowArrayToBeNull(true))
-	require.NoError(err)
+	apiType := getAPITypeFromString(`
+type: string[]
+	`)
+	require.NoError(testCheckValueAPIType(apiType, []string{"text"}))
+	require.Error(testCheckValueAPIType(apiType, nil))
+	require.NoError(testCheckValueAPIType(apiType, nil, CheckValueOptionAllowArrayToBeNull(true)))
 }
 
 func Test_CheckValueAPIType_ArrayInObject(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
-	apiType := APIType{}
-	err = yaml.Unmarshal([]byte(strings.TrimSpace(`
+	apiType := getAPITypeFromString(`
 type: object
 properties:
     strs: string[]
-	`)), &apiType)
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, nil)
-	require.Error(err)
-	err = testCheckValueAPIType(apiType, nil, CheckValueOptionAllowArrayToBeNull(true))
-	require.NoError(err)
+	`)
+	require.Error(testCheckValueAPIType(apiType, nil))
+	require.NoError(testCheckValueAPIType(apiType, nil, CheckValueOptionAllowArrayToBeNull(true)))
 
 	strsObj := map[string]interface{}{
 		"strs": nil,
 	}
-	err = testCheckValueAPIType(apiType, strsObj)
-	require.Error(err)
-	err = testCheckValueAPIType(apiType, strsObj, CheckValueOptionAllowArrayToBeNull(true))
-	require.NoError(err)
+	require.Error(testCheckValueAPIType(apiType, strsObj))
+	require.NoError(testCheckValueAPIType(apiType, strsObj, CheckValueOptionAllowArrayToBeNull(true)))
 }
 
 func Test_CheckValueAPIType_ObjectArray(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
-	apiType := APIType{}
-	apiType.setType("object[]")
-	apiType.Properties = Properties{}
-
-	property := &Property{}
-	property.Name = "text"
-	property.setType(TypeString)
-	addProperty(&apiType.Properties, property)
-
-	err = testCheckValueAPIType(apiType, []interface{}{
-		map[string]interface{}{
-			"text": "",
+	apiType := getAPITypeFromString(`
+type: object[]
+properties:
+    text: string
+	`)
+	require.Error(testCheckValueAPIType(apiType,
+		[]interface{}{
+			map[string]interface{}{
+				"text": "",
+			},
 		},
-	})
-	require.NoError(err)
-
-	err = testCheckValueAPIType(apiType, []interface{}{
-		map[string]interface{}{
-			"text": 0,
+	))
+	require.NoError(testCheckValueAPIType(apiType,
+		[]interface{}{
+			map[string]interface{}{
+				"text": "",
+			},
 		},
-	})
-	require.Error(err)
+		CheckValueOptionAllowRequiredPropertyToBeEmpty(true),
+	))
+	require.NoError(testCheckValueAPIType(apiType,
+		[]interface{}{
+			map[string]interface{}{
+				"text": "test string",
+			},
+		},
+	))
+	require.Error(testCheckValueAPIType(apiType,
+		[]interface{}{
+			map[string]interface{}{
+				"text": 0,
+			},
+		},
+	))
 }
 
 func Test_CheckValueAPIType_CustomType(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
 	parser := NewParser()
 	require.NotNil(parser)
 
-	_, err = parser.ParseData([]byte(strings.TrimSpace(`
+	_, err := parser.ParseData([]byte(strings.TrimSpace(`
 #%RAML 1.0
 types:
     Username:
@@ -305,14 +249,13 @@ types:
 }
 
 func Test_CheckExampleAPIType(t *testing.T) {
-	var err error
 	require := require.New(t)
 	require.NotNil(require)
 
 	parser := NewParser()
 	require.NotNil(parser)
 
-	_, err = parser.ParseData([]byte(strings.TrimSpace(`
+	_, err := parser.ParseData([]byte(strings.TrimSpace(`
 #%RAML 1.0
 types:
     User:
@@ -325,18 +268,4 @@ types:
 	`)), ".")
 	require.Error(err)
 	require.True(ErrorRequiredProperty2.Match(err))
-}
-
-func addProperty(properties *Properties, property *Property) {
-	if properties.mapdata == nil {
-		properties.mapdata = map[string]*Property{}
-	}
-	if properties.propertiesSliceData == nil {
-		properties.propertiesSliceData = []*Property{}
-	}
-	if _, exist := properties.mapdata[property.Name]; exist {
-		return
-	}
-	properties.mapdata[property.Name] = property
-	properties.propertiesSliceData = append(properties.propertiesSliceData, property)
 }
